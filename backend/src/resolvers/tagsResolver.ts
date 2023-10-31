f6ca8c303ed19419c18f3a818f228048ea87a6fb
Pierre Genthon <genthon.pierre@gmail.com>
2023-10-31 16:58:33 +0100

    typegraphql on all ops

--- backend/src/resolvers/tagsResolver.ts
+++ backend/src/resolvers/tagsResolver.ts
@@ -1,18 +1,40 @@
-import { Resolver, Mutation, Arg } from "type-graphql";
+import { Resolver, Mutation, Arg, Query } from "type-graphql";
 import { GraphQLError } from "graphql";
-import { NewTagInput, Tag } from "../entities/tag";
+import { NewTagInput, Tag, UpdateTagInput } from "../entities/tag";
 import { validate } from "class-validator";
+import { Like } from "typeorm";
 
 @Resolver(Tag)
 class TagsResolver {
   @Mutation(() => Tag)
-  async createTag(@Arg("data") { name }: NewTagInput) {
-    const newTag = Tag.create({ name });
-    const errors = await validate(newTag);
-    if (errors.length !== 0)
-      throw new GraphQLError("invalid data", { extensions: { errors } });
-    const newTagWithId = await newTag.save();
-    return newTagWithId;
+  async createTag(@Arg("data", { validate: true }) data: NewTagInput) {
+    const newTag = new Tag();
+    Object.assign(newTag, data);
+    return await newTag.save();
+  }
+  @Query(() => [Tag])
+  async tags(@Arg("name", { nullable: true }) name: string) {
+    return await Tag.find({
+      where: { name: name ? Like(`%${name}%`) : undefined },
+      order: { id: "desc" },
+    });
+  }
+  @Mutation(() => String)
+  async deleteTag(@Arg("tagId") id: number) {
+    const tagToDelete = await Tag.findOneBy({ id });
+    if (!tagToDelete) throw new GraphQLError("not found");
+    await tagToDelete.remove();
+    return "ok";
+  }
+  @Mutation(() => Tag)
+  async updateTag(
+    @Arg("tagId") id: number,
+    @Arg("data", { validate: true }) data: UpdateTagInput
+  ) {
+    const tagToUpdate = await Tag.findOneBy({ id });
+    if (!tagToUpdate) throw new GraphQLError("not found");
+    Object.assign(tagToUpdate, data);
+    return await tagToUpdate.save();
   }
 }
 
