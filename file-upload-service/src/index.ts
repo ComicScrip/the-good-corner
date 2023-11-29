b67ada9f7a97c22d87aa19decbc1d3c8fe83a654
Pierre Genthon <genthon.pierre@gmail.com>
2023-11-29 17:56:30 +0100

    image upload

---
+++ file-upload-service/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+
+const app = new Hono();
+
+app.get("/", (c) =>
+  c.html(
+    `<p>To use this service, POST on ${c.req.url + "uploads"} 
+    with a multipart form containing a "file" field with your file to store.</p>`
+  )
+);
+
+app.use("/files/*", serveStatic({ root: "." }));
+
+app.post("/uploads", async (c) => {
+  const { file }: { file: File } = await c.req.parseBody();
+  const path = "files/" + Date.now() + "-" + file.name;
+  try {
+    console.log("writing");
+    await Bun.write(path, file);
+  } catch (err) {
+    console.error(err);
+  }
+  return c.json({ path, url: c.req.url.replace("uploads", path) });
+});
+
+console.log("server ready");
+
+export default {
+  port: process.env.PORT || 8000,
+  fetch: app.fetch,
+};
